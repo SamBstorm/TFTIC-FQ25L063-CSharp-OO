3e3a1a20be0c6f14bf6a4c4e10c59eeb4c5f676a
Samuel Legrain <samuel.legrain@bstorm.be>
2025-06-24 10:24:41 +0200

    Ajout Correction Banque - Propriétés

---
+++ Exo-ProjetBanque/Models/Courant.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_ProjetBanque.Models
+{
+    internal class Courant
+    {
+        private double _ligneDeCredit;
+
+        public string Numero { get; set; }
+        public Personne Titulaire { get; set; }
+        public double Solde { get; private set; }
+        public double LigneDeCredit
+        {
+            get { return _ligneDeCredit; }
+            set {
+                if (value < 0) return;  //Placer une exception avec un message d'erreur au lieu du return
+                _ligneDeCredit = value; 
+            }
+        }
+
+        public void Depot(double montant)
+        {
+            if(montant <= 0) return;  //Placer une exception avec un message d'erreur au lieu du return
+            Solde += montant;
+        }
+        public void Retrait(double montant)
+        {
+            if (montant <= 0) return;  //Placer une exception avec un message d'erreur au lieu du return
+            if (Solde + LigneDeCredit < montant) return;  //Placer une exception avec un message d'erreur au lieu du return
+            Solde -= montant;
+        }
+    }
+}
---
+++ Exo-ProjetBanque/Program.cs
@@ -0,0 +1,50 @@
+﻿using Exo_ProjetBanque.Models;
+
+namespace Exo_ProjetBanque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Personne p = new Personne()
+            {
+                Nom = "Legrain",
+                Prenom = "Samuel",
+                DateNaiss = new DateTime(1987,9,27)
+            };
+
+            Courant c1 = new Courant() { 
+                Numero = "BE01",
+                LigneDeCredit = -50,
+                Titulaire = p
+            };
+
+            Console.WriteLine(
+@$"Compte courant {c1.Numero}
+Titulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom}
+Solde actuel : {c1.Solde} €
+Ligne de crédit attribué : {c1.LigneDeCredit} €"
+            );
+
+            c1.Depot(100);
+
+            Console.WriteLine(
+@$"Compte courant {c1.Numero}
+Titulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom}
+Solde actuel : {c1.Solde} €
+Ligne de crédit attribué : {c1.LigneDeCredit} €"
+            );
+
+            c1.Retrait(200);
+
+            Console.WriteLine(
+@$"Compte courant {c1.Numero}
+Titulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom}
+Solde actuel : {c1.Solde} €
+Ligne de crédit attribué : {c1.LigneDeCredit} €"
+            );
+        }
+    }
+}
